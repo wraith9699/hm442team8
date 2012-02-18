5b424735db2e7e815cac95828e424c4b700d5351
Greatjob.djs@gmail.com <Greatjob.djs@gmail.com@37ef5540-1036-f9e5-bb95-66ce0492da2b>
2012-02-18 20:25:34 +0000

    Added implementations of the strategies to strategy objects.

--- trunk/src/vital/RespiratoryRate.java
+++ trunk/src/vital/RespiratoryRate.java
@@ -1,5 +1,16 @@
 package vital;
 
-public class RespiratoryRate {
+public class RespiratoryRate implements VitalAlgorithm {
+
+	//TODO: This should be re-designed to pull min/max data for the vital from
+	//the patient. These are included as placeholders.
+	int MIN = 10;
+	int MAX = 40;
+	
+	@Override
+	public int getValue(int rawMeasure) {
+		int realValue = MIN + ((MAX - MIN) * (rawMeasure/100) );
+		return realValue;
+	}
 
 }
