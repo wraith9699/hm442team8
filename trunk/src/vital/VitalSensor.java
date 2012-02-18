a15f60d00e616df94eb455aeba2289f16b45fdfd
Greatjob.djs@gmail.com <Greatjob.djs@gmail.com@37ef5540-1036-f9e5-bb95-66ce0492da2b>
2012-02-18 20:58:15 +0000

    More implementations!

--- trunk/src/vital/VitalSensor.java
+++ trunk/src/vital/VitalSensor.java
@@ -1,5 +1,43 @@
 package vital;
 
-public class VitalSensor {
+import java.util.ArrayList;
 
+public class VitalSensor {
+	
+	ArrayList<VitalData> trackedVitals;
+	
+	public VitalSensor(){
+		trackedVitals = new ArrayList<VitalData>();
+		//Add tracked values with default measures.
+		trackedVitals.add(new VitalData("Blood Pressure", 0));
+		trackedVitals.add(new VitalData("Body Temperature", 0));
+		trackedVitals.add(new VitalData("HearthRate", 0));
+		trackedVitals.add(new VitalData("RespiratoryRate", 0));
+		trackedVitals.add(new VitalData("Weight", 0));
+	}
+	
+	public ArrayList<VitalData> getVitals(){
+		
+		ArrayList<VitalData> vitals = new ArrayList<VitalData>();
+		
+		
+		
+		return vitals;
+	}
+	
+}
+
+
+//Internal context class for the strategy pattern.
+class VitalType {
+	
+	public VitalAlgorithm vitalStrategy;
+	
+	public VitalType(VitalAlgorithm vA){
+		vitalStrategy = vA;
+	}
+	
+	public int getValue(int rawValue){
+		return vitalStrategy.getValue(rawValue);	
+	}
 }
