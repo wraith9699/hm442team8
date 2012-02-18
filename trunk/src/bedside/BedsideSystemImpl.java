0635f21fa26017a24bf305cb6c39bb3431b98f76
Greatjob.djs@gmail.com <Greatjob.djs@gmail.com@37ef5540-1036-f9e5-bb95-66ce0492da2b>
2012-02-18 20:03:40 +0000

    Added stubs for Bedside implementation (interface, stub and impl)/

---
+++ trunk/src/bedside/BedsideSystemImpl.java
@@ -0,0 +1,55 @@
+package bedside;
+
+//The concrete implementation class for the bedside system.
+
+public class BedsideSystemImpl implements BedsideSystem {
+
+	@Override
+	public void getVitals() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTimerPeriods(int period) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void startSensors() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updatePatientVital() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resetAlarm() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void triggerAlarm() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void TriggerCall() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resetCall() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
