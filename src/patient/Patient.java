f2f1d0a4392a06006e85e746d9c06befa17f85df
gdm2034@g.rit.edu <gdm2034@g.rit.edu@37ef5540-1036-f9e5-bb95-66ce0492da2b>
2012-02-18 23:08:20 +0000

    added patient id number

--- src/patient/Patient.java
+++ src/patient/Patient.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 
 public class Patient {
 	private String name;
+	private int patientID;
 	private ArrayList<Vital> vitalList = new ArrayList<Vital>();
 
-	public Patient(String name, ArrayList vitalList ){
+	public Patient(String name, ArrayList vitalList, int idNumber ){
 		this.name = name;
 		this.vitalList = vitalList;
+		this.patientID = idNumber;
 	}
 	
 	public void addVital( Vital newVital){
