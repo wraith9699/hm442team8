a7b9696b6f039b27a83e431582ec941290d3b5fb
Greatjob.djs@gmail.com <Greatjob.djs@gmail.com@37ef5540-1036-f9e5-bb95-66ce0492da2b>
2012-02-18 20:03:40 +0000

    Added stubs for Bedside implementation (interface, stub and impl)/

---
+++ src/bedside/BedsideSystem.java
@@ -0,0 +1,17 @@
+package bedside;
+
+//Top-level interface for the bedside system.
+
+public interface BedsideSystem {
+
+	void getVitals();
+	void setTimerPeriods(int period);  //in milliseconds
+	//Commands w/ no return
+	void startSensors();
+	void updatePatientVital();
+	void resetAlarm();
+	void triggerAlarm();
+	void TriggerCall();
+	void resetCall();
+	
+}
