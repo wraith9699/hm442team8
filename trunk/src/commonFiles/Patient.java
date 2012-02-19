a1b6f5ac0355c205373afc3e1d9d2f49de9da61b
gdm2034@g.rit.edu <gdm2034@g.rit.edu@37ef5540-1036-f9e5-bb95-66ce0492da2b>
2012-02-19 21:06:24 +0000

    updated patient

--- trunk/src/commonFiles/Patient.java
+++ trunk/src/commonFiles/Patient.java
@@ -2,6 +2,7 @@ package commonFiles;
 
 //Test
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Patient {
@@ -12,12 +13,25 @@ public class Patient {
 	public Patient(String name, String patientID){
 		this.name = name;
 		this.patientID = patientID;
+		vitalList.put("bloodPressure", new Vital("bPressure", 0));
+		vitalList.put("bodyTemperature", new Vital("bPressure", 0));
+		vitalList.put("heartRate", new Vital("hRate", 0));
+		vitalList.put("respiratoryRate", new Vital("rRate", 0));
+		vitalList.put("weight", new Vital("weight", 0));
 	}
 	
 	public Patient(String name, String patientID, HashMap vitals){
 		this.name = name;
 		this.patientID = patientID;
 		this.vitalList = vitals;
+	}
+	
+	public void updateCurrentVitals(ArrayList vitalData){
+		((Vital) vitalList.get("bloodPressure")).setCurrent((Integer) vitalData.get(0));
+		((Vital) vitalList.get("bodyTemperature")).setCurrent((Integer) vitalData.get(1));
+		((Vital) vitalList.get("heartRate")).setCurrent((Integer) vitalData.get(2));
+		((Vital) vitalList.get("respiratoryRate")).setCurrent((Integer) vitalData.get(3));
+		((Vital) vitalList.get("weight")).setCurrent((Integer) vitalData.get(4));
 	}
 	
 	public void addVital(Vital newVital){
